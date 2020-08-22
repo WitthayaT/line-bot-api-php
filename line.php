b23dab253816fe2690f6e1d93619b38f5259dbf1
WitthayaT <70052065+WitthayaT@users.noreply.github.com>
2020-08-22 15:30:49 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'ITC8i4J2+XwP18k13Z9SvhcWrEIXtjZ9gj9Qn/BXG9S7doZgHwnN3y42OpJVdkojOFe1Ku+wNKlzduvlX60Oj9s/9USRBlFUu9hpbytQdDIACkn5y8ThE/YWVNHD3GrL2ksaNd93BNubtRMWAZsAwAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
